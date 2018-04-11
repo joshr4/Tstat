7231610cf95cfdf9294ee73d0044efe09d0a7e65
Josh Remaley <joshremaley@gmail.com>
2018-04-11 15:07:42 -0500

    setup

--- server/index.js
+++ server/index.js
@@ -2,9 +2,7 @@ const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const PORT = process.env.PORT || 8080;
 const app = express();
-const server = app.listen(PORT, () => console.log(`Feeling chatty on port ${PORT}`));
 
 module.exports = app;
 
