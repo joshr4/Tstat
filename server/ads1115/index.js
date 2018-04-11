e48d468cab36a81adf94ec0729c0a09fa8cc1d53
Josh Remaley <joshremaley@gmail.com>
2018-04-11 15:41:10 -0500

    test

--- server/ads1115/index.js
+++ server/ads1115/index.js
@@ -2,21 +2,21 @@ const ads1x15 = require('node-ads1x15');
 const chip = 1; //0 for ads1015, 1 for ads1115  
 
 //Simple usage (default ADS address on pi 2b or 3):
-const adc = new ads1x15(chip);
+//const adc = new ads1x15(chip);
 
 // Optionally i2c address as (chip, address) or (chip, address, i2c_dev)
 // So to use  /dev/i2c-0 use the line below instead...:
 
-//    var adc = new ads1x15(chip, 0x48, 'dev/i2c-0');
+var adc = new ads1x15(chip, 0x48, 'dev/i2c-0');
 
-const channel = 0; //channel 0, 1, 2, or 3...  
+//const channel = 0; //channel 0, 1, 2, or 3...  
 const samplesPerSecond = '250'; // see index.js for allowed values for your chip  
 const progGainAmp = '4096'; // see index.js for allowed values for your chip  
 
 //somewhere to store our reading   
-var reading = 0;
+//var reading = 0;
 
-adc.readCh0 = () => {
+adc.read = (channel) => {
     if (!adc.busy) {
         adc.readADCSingleEnded(channel, progGainAmp, samplesPerSecond, function (err, data) {
             if (err) {
