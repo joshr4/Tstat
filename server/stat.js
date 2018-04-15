7bc85483747fc6d34c39670c67686aeae7826fe2
Josh Remaley <joshremaley@gmail.com>
2018-04-15 14:10:56 -0500

    adjust timing

--- server/stat.js
+++ server/stat.js
@@ -4,15 +4,19 @@ var stat = {}
 
 stat.update = () => {
     adc.ch0()
-    .then(data => {
-        stat.ch0 = data
-        console.log(stat.ch0)
-    });
-    
+        .then(data => {
+            stat.ch0 = data
+            console.log('ch0 ', stat.ch0)
+        });
+    setTimeout(() => adc.ch1()
+        .then(data => {
+            stat.ch1 = data
+            console.log('ch1 ', stat.ch1)
+        }), 1500)
 }
 
 stat.start = () => {
-    setInterval(stat.update, 1500)
+    setInterval(stat.update, 4000)
 }
 
 module.exports = stat;
