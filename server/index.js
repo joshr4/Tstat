b94812308a583dfa6b23e7691618fbb9e7e311fa
Josh Remaley <joshremaley@gmail.com>
2018-04-15 13:47:59 -0500

    edits

--- server/index.js
+++ server/index.js
@@ -4,12 +4,16 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const app = express();
 const adc = require('./ads1115/')
+const PORT = 8080 //|| process.env.PORT;
 // const adc = require('./ads1115/')
 
 //setInterval(() => console.log(adc.read()), 1000)
 
 
 module.exports = app;
+
+app.listen(PORT, () => console.log(`Change the temp on port ${PORT}`));
+
 
 // logging middleware
 app.use(morgan('dev'));
