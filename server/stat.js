5da4c63bca64383cc0af552513a1849aa4f537f4
Josh Remaley <joshremaley@gmail.com>
2018-04-15 14:13:58 -0500

    changed

--- server/stat.js
+++ server/stat.js
@@ -5,18 +5,17 @@ var stat = {}
 stat.update = () => {
     adc.ch0()
         .then(data => {
-            stat.ch0 = data
-            console.log('ch0 ', stat.ch0)
+            stat.ch0.raw = data
+            adc.ch1()
+                .then(data => {
+                    stat.ch1.raw = data;
+                    console.log(stat)
+                })
         });
-    setTimeout(() => adc.ch1()
-        .then(data => {
-            stat.ch1 = data
-            console.log('ch1 ', stat.ch1)
-        }), 1500)
 }
 
 stat.start = () => {
-    setInterval(stat.update, 4000)
+    setInterval(stat.update, 3000)
 }
 
 module.exports = stat;
