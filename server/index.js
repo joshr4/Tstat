9bcb320ed721827a9b2f1dfd32796e4b1b709c06
Josh Remaley <joshremaley@gmail.com>
2018-04-15 13:47:12 -0500

    edit to main file

--- server/index.js
+++ server/index.js
@@ -4,7 +4,6 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const app = express();
 const adc = require('./ads1115/')
-const PORT = 8080 //|| process.env.PORT;
 // const adc = require('./ads1115/')
 
 //setInterval(() => console.log(adc.read()), 1000)
