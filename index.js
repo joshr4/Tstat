391046c570e549943469a1bb07ebea96ab6117f6
Josh Remaley <joshremaley@gmail.com>
2018-04-15 22:13:18 -0500

    update

--- index.js
+++
@@ -1,4 +0,0 @@
-const app = require('../server/')
-const PORT = 8080 //|| process.env.PORT;
-
-app.listen(PORT, () => console.log(`Change the temp on port ${PORT}`));
---
+++ server/server.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const app = express();
+const adc = require('./ads1115/')
+const PORT = 8080 //|| process.env.PORT;
+const stat = require('./stat')
+// const adc = require('./ads1115/')
+
+//setInterval(() => console.log(adc.read()), 1000)
+
+
+module.exports = app;
+
+//app.listen(PORT, () => console.log(`Change the temp on port ${PORT}`))
+
+stat.start() //starts up the thermostat logic loops
+
+
+// logging middleware
+app.use(morgan('dev'));
+
+// static middleware
+app.use(express.static(path.join(__dirname, '..', 'node_modules')));
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+// body parsing middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// 'API' routes
+app.use('/api', require('./api'));
+
+// 404 middleware
+app.use((req, res, next) =>
+  path.extname(req.path).length > 0 ?
+    res.status(404).send('Not found') :
+    next()
+);
+
+// send index.html
+app.use('*', (req, res, next) =>
+  res.sendFile(path.join(__dirname, '..', 'public/index.html'))
+);
+
+// error handling endware
+app.use((err, req, res, next) =>
+  res.status(err.status || 500).send(err.message || 'Internal server error.')
+);
